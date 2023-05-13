70e5a5b955d4b0fbce151bf174c53157be710ed4
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-13 02:56:31 +0200

    copied the example for the color.cpp into the tryout file and edited the CMakeLists.txt file so everything compiles and works

--- cpp-terminal_tryout/build.bat
+++ cpp-terminal_tryout/build.bat
@@ -1,5 +1,5 @@
 echo off
 cls
 echo "---------- [CMake | build | Visual Studio 17 2022 | Release] -----------"
-cmake -S . -B build -G "Visual Studio 17 2022"
-cmake --build build --config Release
+cmake -S . -B build -G "Visual Studio 17 2022" 
+cmake --build build --config Release --target cpp_terminal_tryout
