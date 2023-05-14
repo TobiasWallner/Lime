f1cebb43fc9ca20923a743d47c39a3cc2563dbba
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-14 23:50:57 +0200

    changed the build generator from ninja to mingw make files

--- cpp-terminal_tryout/build_clang.bat
+++ cpp-terminal_tryout/build_clang.bat
@@ -1,7 +1,7 @@
 echo off
 cls
-echo "---------- [CMake | build | Release | Clang | Ninja] -----------"
+echo "---------- [CMake | build | Release | Clang] -----------"
 SET CC=clang
 SET CXX=clang++
-cmake -S . -B build_clang -G "Ninja"  
+cmake -S . -B build_clang -G "MinGW Makefiles"
 cmake --build build_clang --config Release --target cpp_terminal_tryout
