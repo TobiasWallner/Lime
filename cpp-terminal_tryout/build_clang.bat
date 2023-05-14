68fbb67212da9303776713ab37ef97b9f9160d26
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-15 00:36:28 +0200

    changed the build prozess to a multi-config one

--- cpp-terminal_tryout/build_clang.bat
+++ cpp-terminal_tryout/build_clang.bat
@@ -3,5 +3,5 @@ cls
 echo "---------- [CMake | build | Release | Clang] -----------"
 SET CC=clang
 SET CXX=clang++
-cmake -S . -B build_clang -G "Ninja"
+cmake -S . -B build_clang -G "Ninja Multi-Config"
 cmake --build build_clang --config Release --target cpp_terminal_tryout
