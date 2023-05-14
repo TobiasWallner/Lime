42dd62cf2683fe22c6199dff89ef5f60b692bafd
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-14 23:30:09 +0200

    rewrote the build_* scripts so that they are not useing any absolute paths but require to have the user set the environment variables

--- cpp-terminal_tryout/build_clang.bat
+++ cpp-terminal_tryout/build_clang.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 echo "---------- [CMake | build | Release | Clang | Ninja] -----------"
-SET CC=C:\Program Files\LLVM\bin\clang
-SET CXX=C:\Program Files\LLVM\bin\clang++
+SET CC=clang
+SET CXX=clang++
 cmake -S . -B build_clang -G "Ninja"  
 cmake --build build_clang --config Release --target cpp_terminal_tryout
--- cpp-terminal_tryout/build_gcc.bat
+++ cpp-terminal_tryout/build_gcc.bat
@@ -1,7 +1,7 @@
 echo off
 cls
 echo "---------- [CMake | build | Release | GCC | MinGW Makefiles] -----------"
-SET CC=C:\MinGW\bin\gcc
-SET CXX=C:\MinGW\bin\g++
+SET CC=gcc
+SET CXX=g++
 cmake -S . -B build_gcc -G "MinGW Makefiles"  
 cmake --build build_gcc --config Release --target cpp_terminal_tryout
