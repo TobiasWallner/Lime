8e30e86d8c4976b825a925018387f62ae0682272
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-18 18:51:29 +0200

    Made a lime class that after run() is invoked will take care of all inputs. In the future it will also store all GUI elements

---
+++ lime/Lime.cpp
@@ -0,0 +1,127 @@
+
+// Project
+#include "Lime.hpp"
+
+// C std
+#include <stdlib.h>
+#include <string.h>
+
+// C++ std
+#include <iostream>
+#include <cctype>
+#include <cstdint>
+
+// cpp-terminal
+#include <cpp-terminal/cursor.hpp>
+#include <cpp-terminal/input.hpp>
+#include <cpp-terminal/io.hpp>
+#include <cpp-terminal/key.hpp>
+#include <cpp-terminal/options.hpp>
+#include <cpp-terminal/screen.hpp>
+#include <cpp-terminal/terminal.hpp>
+#include <cpp-terminal/tty.hpp>
+#include <cpp-terminal/event.hpp>
+
+
+int Lime::run(){
+	
+	// the following should capture keystrokes and prozess them immediatelly
+	// this is just the start of the program that will evolve from this point
+	
+	// check if the input terminal is attatched to the console
+	if(Term::is_stdin_a_tty() == false){
+		std::cout << "Error: terminal does not have an input stream ... exiting program" << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	// set options for the console
+	const auto Options = {
+		Term::Option::NoClearScreen, 
+		Term::Option::NoSignalKeys, 
+		Term::Option::Cursor, 
+		Term::Option::Raw
+	};
+	
+	Term::terminal.setOptions(Options);
+	
+	// Entering the Program loop: get inputs -> prozess inputs -> display/save changes
+	this->run_main_loop();
+	
+	return EXIT_SUCCESS;
+}
+
+void Lime::quit(){
+	/**
+		Savely quit the program.
+	*/
+	std::cout << "Quitting Lime" << std::endl;
+	this->main_loop_continue = false;
+}
+
+void Lime::run_main_loop(){
+	while(this->main_loop_continue){
+		auto event = Term::read_event();
+		this->prozess_event(std::move(event));
+	}
+}
+
+
+void Lime::prozess_event(Term::Event&& event){
+	switch(event.type()){
+		case Term::Event::Type::Empty : {
+			this->prozess_empty_event();
+		}return;
+		case Term::Event::Type::Key : {
+			const auto keyEvent = Term::Key(event);
+			this->prozess_key_event(keyEvent);
+		}return;
+		case Term::Event::Type::Screen : {
+			this->prozess_screen_event();
+		}return;
+		case Term::Event::Type::Cursor : {
+			this->prozess_cursour_event();	
+		}return;
+		case Term::Event::Type::CopyPaste : {
+			this->prozess_copy_paste_event();
+		}return; 
+		default : {
+			this->prozess_unhandeled_event(std::move(event));
+		}return;
+	}
+}
+
+void Lime::prozess_key_event(Term::Key keyEvent){
+	// for now just print the key if it is printable to the console
+	const auto c = static_cast<char32_t>(keyEvent + Term::Key::NUL); 
+	//(Tobias): aparently you have to add the NUL type I found no other way of doing typeconversions
+	if(std::isprint(c)){
+		std::cout << static_cast<char>(c) << std::flush; 
+		//c++ std library cannot really print utf-8 or char32_t characters so I leave it at char types for now
+	}else{
+		this->quit();
+	}
+}
+  
+  
+void Lime::prozess_empty_event(){
+	std::cout << "Internal Error: Unhandeled Event type 'Empty' " << std::endl;
+}
+
+void Lime::prozess_screen_event(){
+	std::cout << "Internal Error: Unhandeled Event type 'Screen' " << std::endl;
+}
+
+void Lime::prozess_cursour_event(){
+	std::cout << "Internal Error: Unhandeled Event type 'Cursor' " << std::endl;
+}
+
+void Lime::prozess_copy_paste_event(){
+	std::cout << "Internal Error: Unhandeled Event type 'CopyPaste' " << std::endl;
+}
+
+void Lime::prozess_unhandeled_event(Term::Event&& event){
+	/*TODO: a way to display error messages that do not disturb the writeing prozess
+		For example by message windows or at a special place at the bottom of the screen
+	*/
+	std::cout << "Internal Error: Unhandeled Event type ID: " << static_cast<int>(event.type()) << std::endl;
+}
---
+++ lime/Lime.hpp
@@ -0,0 +1,99 @@
+
+#include <cpp-terminal/event.hpp>
+
+class Lime{
+	
+	bool main_loop_continue = true;
+	
+public:
+	
+	constexpr Lime() = default;
+	
+	/**
+		The Copy consturctor of the class Lime is deleted, because the 
+		it may hold open files or handels that should not be copied but 
+		moved instead.
+	*/
+	Lime(const Lime&) = delete;
+	Lime& operator = (const Lime&) = delete;
+	
+	/**
+		Enable default move constructor
+	*/
+	Lime(Lime&&) = default;
+	Lime& operator = (Lime&&) = default;
+	
+	/**
+		run the lime text editor with the provided console arguments
+		
+		Returns:
+		--------
+		* returns EXIT_SUCCESS if the editor finished and closes without any errors.
+		* returns EXIT_SUCCESS if errors appeared that the editor could handle with or
+			without the help of some user input
+		* returns EXIT_FAILURE if an error occured that the editor can do nothing about.
+			like un-mounted input and output streams to the console.
+	*/
+	int run();
+
+	/**
+		Savely quits the editor from the current state.
+		May asks/prompts the user to save unsaved files before 
+		quitting and canceles the quitting event.
+		
+		(May feed the quit event in the received events.)
+	*/
+	void quit();
+	
+private:
+
+	/**
+		the main loop does three things
+			1. wait for and event/input
+			2. prozess the input
+			3. display the output (may already be included in prozess input)
+	*/
+	void run_main_loop();
+	
+	/**
+		Prozesses any event like a key press or the resizeing of the console window and
+		passes them to the corresponding event handlers.
+	*/
+	void prozess_event(Term::Event&& event);
+	
+	/**
+		Prozesses a key event and passes it to the GUI object that is currently in focus
+		and has the active cursor. 
+	*/
+	void prozess_key_event(Term::Key keyEvent);	
+	
+	/**
+		TODO: do more research what this event does
+	*/
+	void prozess_empty_event();
+	
+	/**
+		Prozesses a screen event, like the resizeing of the console window and passes the 
+		new length and width recursively to all GUI elements so that they can dynamically
+		resize and adapt to the new avaliable screen size.
+	*/
+	void prozess_screen_event();
+	
+	/**
+		Prozesses a cursour event
+	*/
+	void prozess_cursour_event();
+	
+	/**
+		Prozesses a copy paste event
+	*/
+	void prozess_copy_paste_event();
+	
+	/**
+		Event handler that handles any un-handled events by for example displaying an 
+		error message somewhere on the screen and maybe additionally print a log file
+		with further information about the event type.
+	*/
+	void prozess_unhandeled_event(Term::Event&& event);
+		
+};
