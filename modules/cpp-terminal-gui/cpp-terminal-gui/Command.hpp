02e960bf37f42cf47d9f3536bb09062f01c2eb68
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-24 19:40:21 +0200

    added an enum for the backgound color type, removed the type member from the abstract class and instead use a virtual function call with the same name, added two commands ForegroundColor and BackgroundColor beneath the Comman class

--- modules/cpp-terminal-gui/cpp-terminal-gui/Command.hpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/Command.hpp
@@ -6,6 +6,9 @@
 // Project
 #include "RenderTrait.hpp"
 
+//cpp-terminal
+#include <cpp-terminal\color.hpp>
+
 namespace TermGui{
 
 /**
@@ -14,7 +17,8 @@ namespace TermGui{
 	the following class 'Command'.
 */
 enum class CommandType{
-	ForgroundColour,
+	forgroundColor,
+	backgroundColor,
 };
 
 /**
@@ -27,23 +31,52 @@ enum class CommandType{
 	
 */
 class Command : public RenderTrait{
-private:
-	const CommandType _type;
 public:
 
-	/// The default constructor of this class is deleted - cannot default construct this class
-	CommandType() = delete;
+	/// default constructable
+	Command() = default;
 	
 	virtual ~Command(){}
 	
-	/// One may only construct an instance of this class by telling it its Command type
-	explicit constexpr inline CommandType(CommandType type) : _type(type) {}
-	
 	/// The derived call has to specifiy how the specific Command gets rendered to a string command for the terminal
-	virtual render(std::string& outputString) = 0;
+	virtual void render(std::string& outputString) const override = 0;
 	
 	/// returns the command type (enum)
-	constexpr inline CommandType type(){ return this->_type;}
+	virtual CommandType type() const = 0;
+};
+
+
+//
+// +++++++++++++++++++ Below is the list of basic commands ++++++++++++++++++++
+//
+
+/// The forground color is a command that can be put in a command list and will then be executed by the list
+class ForegroundColor : public Command{
+private:
+	Term::Color _color;
+	
+public:
+	inline ForegroundColor(Term::Color color) : _color(color){}
+	inline ForegroundColor(Term::Color::Name colorName) : _color(colorName){}
+	inline ForegroundColor(std::uint8_t value) :  _color(value){}
+	inline ForegroundColor(std::uint8_t red, std::uint8_t green, std::uint8_t blue) : _color(red, blue, green){}
+	
+	inline void render(std::string& output_string) const override {Term::color_fg(this->_color);}
+	inline CommandType type() const override {return CommandType::forgroundColor;}
+};
+
+class BackgroundColor : public Command{
+private:
+	Term::Color _color;
+	
+public:
+	inline BackgroundColor(Term::Color color) : _color(color){}
+	inline BackgroundColor(Term::Color::Name colorName) : _color(colorName){}
+	inline BackgroundColor(std::uint8_t value) : _color(value){}
+	inline BackgroundColor(std::uint8_t red, std::uint8_t green, std::uint8_t blue) : _color(red, blue, green){}
+	
+	inline void render(std::string& output_string) const override {Term::color_bg(this->_color);}
+	inline CommandType type() const override {return CommandType::backgroundColor;}
 };
 
 }
