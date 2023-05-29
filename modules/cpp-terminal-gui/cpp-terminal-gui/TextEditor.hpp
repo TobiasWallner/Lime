75d1626e86c654e5c6ccc00c19eafa772a07037b
Tobias Wallner <tobias.wallner1@gmx.net>
2023-05-29 12:18:10 +0200

    added iterators, added insertions, added functions to move the cursor/insertion position, added functions that tell weher you are in the file

--- modules/cpp-terminal-gui/cpp-terminal-gui/TextEditor.hpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/TextEditor.hpp
@@ -6,6 +6,10 @@
 // cpp-terminal
 #include <cpp-terminal/cursor.hpp>
 
+// utf8 string 
+#include <utf8_char.hpp>
+#include <utf8_string.hpp>
+
 #include "ColorString.hpp"
 
 namespace TermGui{
@@ -20,6 +24,8 @@ public:
 	using size_type = std::size_t;
 	using iterator = Text::iterator;
 	using const_iterator = Text::const_iterator;
+	using reverse_iterator = Text::reverse_iterator;
+	using const_reverse_iterator = Text::const_reverse_iterator;
 	using reference = Text::const_reference;
 	using const_reference = Text::const_reference;
 	using value_type = Text::value_type;
@@ -44,6 +50,41 @@ public:
 	/// constructs a text with one empty line + sets the cursor position and iterators
 	TextEditor();
 	
+	iterator begin(){return this->_text.begin();}
+	const_iterator begin() const {return this->_text.cbegin();}
+	const_iterator cbegin() const {return this->_text.cbegin();}
+	
+	iterator lineItr(){return this->_cursor.lineIterator;}
+	const_iterator lineItr() const {return this->_cursor.lineIterator;}
+	const_iterator clineItr() const {return this->_cursor.lineIterator;}
+	
+	iterator end(){return this->_text.end();}
+	const_iterator end() const {return this->_text.cend();}
+	const_iterator cend() const {return this->_text.cend();}
+	
+	iterator last(){return this->rbegin().base();}
+	const_iterator last() const {return this->crbegin().base();}
+	const_iterator clast() const {return this->crbegin().base();}
+	
+	reverse_iterator rbegin(){return this->_text.rbegin();}
+	const_reverse_iterator rbegin() const {return this->_text.crbegin();}
+	const_reverse_iterator crbegin() const {return this->_text.crbegin();}
+	
+	reverse_iterator rend(){return this->_text.rend();}
+	const_reverse_iterator rend() const {return this->_text.crend();}
+	const_reverse_iterator crend() const {return this->_text.crend();}
+	
+	/// inserts a character at the current cursor position into the string
+	void insert(utf8::Char c);
+	
+	/// converts the char into a utf8::Char and inserts it at the current cursor position and advances the cursor
+	inline void insert(char c){this->insert(utf8::Char(c));}
+	
+	/// inserts the null terminated string into the stream
+	/// returns true is the string could be read as utf8, returns false otherwise
+	bool insert(const char*);
+	
+	
 	/// returns the line position of the cursor
 	inline size_type get_line() const {return this->_cursor.lineNumber;}
 	
@@ -51,7 +92,17 @@ public:
 	inline size_type get_column() const {return this->_cursor.columnNumber;}
 	
 	
+	/// moves the cursor to the rigth, aka. advances the column by one.
+	/// if at the end of line perform a jump to the next line, if it exists
+	void move_right();
+
+	inline bool is_start_of_line() const {return this->get_column() == 0;}
+
+	inline bool is_end_of_line() const {return this->get_column() == this->lineItr()->size();}
 	
+	inline bool is_last_line() const {return this->lineItr() == this->last();}
+
+	inline bool is_end_of_file() const { return this->is_last_line() && this->is_end_of_line(); }
 	
 	
 private:
