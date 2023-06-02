60410fe040c6f76fc6131a81fc230f4511146285
Helena Schneider <e11812332@student.tuwien.ac.at>
2023-06-02 20:56:59 +0200

    files are now arguments of read and write functions

--- readWrite.cpp
+++ readWrite.cpp
@@ -10,26 +10,19 @@ use something like:
 #include <iostream>
 #include <list>
 
-// TODO(Helena): never, ever write the following line in a header file!
 using namespace std;
 
-/*	TODO(Helena): the functions you wrote is better now but will produce linker errors
-	either do one of the following: mark them as 'inline' or put them in a .cpp file.
-*/
 //Writes a file into a list, each line is a string
-void readFile(std::list<std::string> &list){
-    ifstream file ("data.txt");
+void readFile(std::list<std::string> &list, ifstream &file){
     while(file.good()){
         string line;
         getline(file, line);
         list.push_back(std::move(line));
     }
 }
-
-// TODO(Helena): make the output file name a parameter of the function 
+ 
 //Writes a doubly linked list into a file
-void writeFile(std::list<std::string> &list){
-    ofstream file ("data.txt");
+void writeFile(std::list<std::string> &list, ofstream &file){
     if(file.is_open()){
         for(string line : list){
             file << line << "\n";
