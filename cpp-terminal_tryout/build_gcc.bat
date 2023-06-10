59c21a61869d305dcd530333fc9cd1f10512765a
Tobias Wallner <tobias.wallner1@gmx.net>
2023-06-10 13:46:25 +0200

    deleted the documentation function that was intended for the library we use but never wrote anything. + deleted the cpp-termina_tryout folder since we know how the library works now and the tryout phase is over

--- cpp-terminal_tryout/build_gcc.bat
+++
@@ -1,7 +0,0 @@
-echo off
-cls
-echo "---------- [CMake | build | Release | GCC | MinGW Makefiles] -----------"
-SET CC=gcc
-SET CXX=g++
-cmake -S . -B build_gcc -G "MinGW Makefiles"  
-cmake --build build_gcc --config Release --target cpp_terminal_tryout
