8741972c610f8dafc8631f65ed5682d6131207ab
Tobias Wallner <tobias.wallner1@gmx.net>
2023-06-12 23:14:11 +0200

    added functions of inserting and deleting/eraseing to the EditTrait

--- modules/cpp-terminal-gui/cpp-terminal-gui/EditTrait.hpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/EditTrait.hpp
@@ -19,10 +19,49 @@ class EditTrait{
 	
 	// ----- edit functions -----
 	
-	virtual clear() = 0
+	virtual void clear() = 0;
 	
-	virtual EditTrait& insert(utf8::Char c) = 0;
+	virtual void insert(utf8::Char c) = 0;
 	
+	inline void insert(char c){this->insert(utf8::Char(c));}
+	
+	inline bool insert(const char* str){
+		while(*str != '\0'){
+			utf8::Char c;
+			const char* next = c.assign(str);
+			if(next == str){
+				// no characters have been read -> error
+				return false;
+			}
+			this->insert(c);
+			str = next;
+		}
+		return true;
+	}
+
+	inline bool insert(const char* str, size_t size){
+		size_t pos = 0;
+		while(pos < size){
+			utf8::Char c;
+			const char* next = c.assign(str, size - pos);
+			pos += std::distance(str, next);
+			if(next == str){
+				// no characters have been read -> error
+				return false;
+			}
+			this->insert(c);
+			str = next;
+		}
+		return true;
+	}
+	
+	virtual void enter() = 0;
+	
+	/// deletes the character at or after the cursor position
+	virtual void erase() = 0;
+	
+	/// deletes the character before the cursor position
+	virtual void Delete() = 0;
 	
 	// ----- cursor functions -----
 	
