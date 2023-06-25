2416186e160b5ae79145bd5560b1871757527bea
Tobias Wallner <tobias.wallner1@gmx.net>
2023-06-25 22:50:30 +0200

    added vertical grids and grid cells

---
+++ modules/cpp-terminal-gui/cpp-terminal-gui/VerticalGrid.cpp
@@ -0,0 +1,311 @@
+
+// C++ std
+#include <cmath>
+#include <algorithm>
+
+// Project
+#include "VerticalGrid.hpp"
+
+//------------------------ VerticalGridCell ---------------------
+
+
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::unique_pointer&& element, 
+											TermGui::ScreenWidth::size_type absoluteHeight) : 
+		ownedElement(std::move(element)),
+		element(ownedElement.get()),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = absoluteHeight},
+		relativeHeight(0.0f),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Absolute){}
+		
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::pointer element, 
+											TermGui::ScreenWidth::size_type absoluteHeight) : 
+		ownedElement(),
+		element(element),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = absoluteHeight},
+		relativeHeight(0.0f),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Absolute){}
+		
+		
+		
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::unique_pointer&& element, 
+											float relativeHeight) : 
+		ownedElement(std::move(element)),
+		element(ownedElement.get()),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = 0},
+		relativeHeight(relativeHeight),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Relative){}
+		
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::pointer element, 
+											float relativeHeight) : 
+		ownedElement(),
+		element(element),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = 0},
+		relativeHeight(relativeHeight),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Relative){}
+		
+		
+
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::unique_pointer&& element, 
+											DynamicToken dynamicToken) : 
+		ownedElement(std::move(element)),
+		element(ownedElement.get()),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = 0},
+		relativeHeight(0.0f),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Absolute){}
+		
+TermGui::VerticalGridCell::VerticalGridCell(TermGui::VerticalGridCell::pointer element, 
+											DynamicToken dynamicToken) : 
+		ownedElement(),
+		element(element),
+		screenPosition{.x = 0, .y = 0},
+		screenWidth{.x = 0, .y = 0},
+		relativeHeight(0.0f),
+		minimalHeight(0),
+		maximalHeight(-1),
+		gridWidthType(GridWidthType::Dynamic){}
+
+void TermGui::VerticalGridCell::render(std::string& output) const {
+	this->element->render(output);
+}
+
+void TermGui::VerticalGridCell::set_screen_position(TermGui::ScreenPosition position){
+	this->screenPosition = position;
+	this->element->set_screen_position(position);
+}
+	
+TermGui::ScreenPosition TermGui::VerticalGridCell::get_screen_position() const{
+	return this->screenPosition;
+}
+
+void TermGui::VerticalGridCell::set_screen_width(TermGui::ScreenWidth width){
+	this->screenWidth = width;
+	this->element->set_screen_width(width);
+}
+
+TermGui::ScreenWidth TermGui::VerticalGridCell::get_screen_width() const{
+	return this->screenWidth;
+}
+
+
+
+//------------------------ VerticalGrid ---------------------
+
+TermGui::ScreenWidth::size_type TermGui::VerticalGrid::accumulate_absolute_cell_height() const{
+	ScreenWidth::size_type sum = 0;
+	for(const VerticalGridCell& elem : this->gridCells){
+		sum += elem.get_height_if_absolute() + elem.get_height_if_dynamic();
+	}
+	return sum;
+}
+	
+float TermGui::VerticalGrid::accumulate_relative_cell_height() const{
+	float sum = 0;
+	for(const VerticalGridCell& elem : this->gridCells){
+		sum += elem.get_height_if_relative();
+	}
+	return sum;
+}
+
+
+void TermGui::VerticalGrid::distribute_cells(){
+	const TermGui::ScreenWidth::size_type absoluteCellHeight = this->accumulate_absolute_cell_height();
+	
+	float relativeCellsHeight = this->accumulate_relative_cell_height();
+	ScreenWidth::size_type remainingScreenHeight = this->screenWidth.y - absoluteCellHeight;
+	
+	ScreenPosition position = this->screenPosition;
+	
+	for(VerticalGridCell& elem : this->gridCells){
+		if(elem.is_relative()){
+			// calculate height of the relative cell
+			const float relativeHeight = elem.get_relative_height() / relativeCellsHeight;
+			const float absoluteHeight = remainingScreenHeight * relativeHeight;
+			const TermGui::ScreenWidth::size_type roundedHeight = static_cast<ScreenWidth::size_type>(std::round(absoluteHeight));
+			const TermGui::ScreenWidth::size_type assignedHeight = std::min(remainingScreenHeight, roundedHeight);
+			
+			// assign height
+			elem.set_screen_width(TermGui::ScreenWidth{this->screenWidth.x, assignedHeight});
+			
+			// post - prozessing and preparation for the next iteration
+			relativeCellsHeight -= relativeHeight;
+			remainingScreenHeight -= assignedHeight;
+		}
+		elem.set_screen_position(position);
+		position.y += elem.get_screen_width().y;
+	}
+}
+
+
+
+
+inline void TermGui::VerticalGrid::insert_absolute(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::unique_pointer&& element, 
+		ScreenWidth::size_type height){
+	this->gridCells.insert(pos, VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+inline void TermGui::VerticalGrid::insert_absolute(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::pointer&& element, 
+		ScreenWidth::size_type height){
+	this->gridCells.insert(pos, VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+inline void TermGui::VerticalGrid::insert_absolute(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::unique_pointer&& element, 
+		ScreenWidth::size_type height){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+inline void TermGui::VerticalGrid::insert_absolute(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::pointer&& element, 
+		ScreenWidth::size_type height){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+
+
+
+
+void TermGui::VerticalGrid::insert_relative(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::unique_pointer&& element, 
+		float height){
+	this->gridCells.insert(pos, VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_relative(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::pointer&& element, 
+		float height){
+	this->gridCells.insert(pos, VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_relative(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::unique_pointer&& element, 
+		float height){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_relative(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::pointer&& element, 
+		float height){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+
+
+
+
+void TermGui::VerticalGrid::insert_dynamic(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::unique_pointer&& element){
+	this->gridCells.insert(pos, VerticalGridCell(std::move(element), TermGui::dynamicToken));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_dynamic(
+		TermGui::VerticalGrid::const_iterator pos, 
+		TermGui::VerticalGrid::pointer&& element){
+	this->gridCells.insert(pos, VerticalGridCell(element, TermGui::dynamicToken));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_dynamic(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::unique_pointer&& element){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(std::move(element), TermGui::dynamicToken));
+	this->distribute_cells();
+}
+
+void TermGui::VerticalGrid::insert_dynamic(
+		TermGui::VerticalGrid::size_type pos, 
+		TermGui::VerticalGrid::pointer&& element){
+	this->gridCells.insert(this->gridCells.cbegin() + pos, VerticalGridCell(element, TermGui::dynamicToken));
+	this->distribute_cells();
+}
+
+
+
+void TermGui::VerticalGrid::push_back_absolute(TermGui::VerticalGrid::unique_pointer&& element, ScreenWidth::size_type height){
+	this->gridCells.push_back(VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+void TermGui::VerticalGrid::push_back_absolute(TermGui::VerticalGrid::pointer element, ScreenWidth::size_type height){
+	this->gridCells.push_back(VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+
+
+
+void TermGui::VerticalGrid::push_back_relative(TermGui::VerticalGrid::unique_pointer&& element, float height){
+	this->gridCells.push_back(VerticalGridCell(std::move(element), height));
+	this->distribute_cells();
+}
+void TermGui::VerticalGrid::push_back_relative(TermGui::VerticalGrid::pointer element,  float height){
+	this->gridCells.push_back(VerticalGridCell(element, height));
+	this->distribute_cells();
+}
+
+
+
+void TermGui::VerticalGrid::push_back_dynamic(TermGui::VerticalGrid::unique_pointer&& element){
+	this->gridCells.push_back(VerticalGridCell(std::move(element), TermGui::dynamicToken));
+	this->distribute_cells();
+}
+void TermGui::VerticalGrid::push_back_dynamic(TermGui::VerticalGrid::pointer element){
+	this->gridCells.push_back(VerticalGridCell(element, TermGui::dynamicToken));
+	this->distribute_cells();
+}
+	
+
+
+void TermGui::VerticalGrid::render(std::string& outputString) const{
+	for(const VerticalGridCell& elem : this->gridCells){
+		elem.render(outputString);
+	}
+}
+
+void TermGui::VerticalGrid::set_screen_position(TermGui::ScreenPosition position){
+	this->screenPosition = position;
+	for(VerticalGridCell& elem : this->gridCells){
+		elem.set_screen_position(position);
+		position.y += elem.get_screen_width().y;
+	}
+}
+	
+TermGui::ScreenPosition TermGui::VerticalGrid::get_screen_position() const{
+	return this->screenPosition;
+}
+
+void TermGui::VerticalGrid::set_screen_width(TermGui::ScreenWidth width){
+	this->screenWidth = width;
+	this->distribute_cells();
+}
+
+TermGui::ScreenWidth TermGui::VerticalGrid::get_screen_width() const{
+	return this->screenWidth;
+}
---
+++ modules/cpp-terminal-gui/cpp-terminal-gui/VerticalGrid.hpp
@@ -0,0 +1,211 @@
+
+// C++ std
+#include <cinttypes>
+#include <memory>
+#include <vector>
+#include <variant>
+
+// cpp-terminal
+#include <cpp-terminal/cursor.hpp>
+
+// project
+#include "RenderTrait.hpp"
+
+
+namespace TermGui{
+
+
+enum class GridWidthType{
+	Absolute,	// set a defined number of lines
+	Relative,	// set a relative unber of lines that will be calculated from the remaining ones
+	Dynamic		// askes the element underneath how many lines it needs
+};
+
+/// Token class with no memory that symbolises that a passed function parameter is dynamic
+class DynamicToken{};
+
+/// Token instance which if passed to a function symbolises that the other parameter is dynamic
+inline constexpr DynamicToken dynamicToken;
+
+/// VerticalGridCell stores some element and additional information for the grid it is in.
+/// can store a 'unique pointer' to an object, that owns the memory,
+/// or a 'raw pointer' that does not store anything
+class VerticalGridCell : public RenderTrait{
+public:
+	using pointer = RenderTrait*;
+	using unique_pointer = std::unique_ptr<RenderTrait>;
+	using size_type = unsigned short;
+	
+
+private:
+	unique_pointer ownedElement; // optionally store the object
+	pointer element;
+	
+	ScreenPosition screenPosition;
+	ScreenWidth screenWidth;
+	
+	GridWidthType gridWidthType;
+	
+	float relativeHeight;
+	size_type minimalHeight;
+	size_type maximalHeight;
+	
+	
+public:
+
+	/// constructs a grid cell with an absolute number of lines
+	VerticalGridCell(unique_pointer&& element, ScreenWidth::size_type absoluteHeight);
+	VerticalGridCell(pointer element, ScreenWidth::size_type absoluteHeight);
+		
+	/// constructs a grid cell with a relative number of lines.
+	/// This means that the number of lines of this cell is proportional 
+	/// to other grid cells in the same grid
+	VerticalGridCell(unique_pointer&& element, float relativeHeight);
+	VerticalGridCell(pointer element, float relativeHeight);
+	
+	/// constructs a grid cell with a dynamic number of lines.
+	/// This means that the number of lines of an element in the cell is 
+	/// defined by the element itself.
+	VerticalGridCell(unique_pointer&& element, DynamicToken dynamicToken);
+	VerticalGridCell(pointer element, DynamicToken dynamicToken);
+		
+	inline void set_grid_width_type(GridWidthType type){this->gridWidthType = type;}
+	
+	inline GridWidthType get_grid_width_type() const {return gridWidthType;}
+	
+	inline bool is_relative() const {return this->gridWidthType == GridWidthType::Relative;}
+	inline bool is_absolute() const {return this->gridWidthType == GridWidthType::Absolute;}
+	inline bool is_dynamic() const {return this->gridWidthType == GridWidthType::Dynamic;}
+	
+	/// returns the absolute height wether it is valid or not
+	/// check is_absolute() to know if it is valid
+	inline ScreenWidth::size_type get_absolute_height() const {return this->screenWidth.y;}
+	
+	/// returns the relative height wether it is valid or not
+	/// check is_relative to know if it is valid
+	inline float get_relative_height() const {return this->relativeHeight;}
+	
+	/// returns the height of the stored object
+	inline ScreenWidth::size_type get_dynamic_height() const {return this->element->get_screen_width().y;}
+	
+	/// returns the absolute height of grid cell if the cell is an absolute cell and 0 otherwise
+	inline ScreenWidth::size_type get_height_if_absolute() const {return (this->is_absolute()) ? this->screenWidth.y : 0;}
+	
+	/// return the relatice height of the grid cell if the cell is a relatice cell and 0 otherwise
+	inline float get_height_if_relative() const {return (!(this->is_relative())) ? this->relativeHeight : 0.0f;}
+	
+	/// return sthe dynamic height of the grid cell if the cell is a dynaic cell and 0 otherwise
+	inline ScreenWidth::size_type get_height_if_dynamic() const {return (!(this->is_dynamic())) ? this->element->get_screen_width().y : 0;}
+	
+	/// makes the cell an absolute cell and sets the height
+	inline void set_absolute_height(ScreenWidth::size_type height){
+		this->gridWidthType = GridWidthType::Absolute;
+		this->screenWidth.y = height;
+	}
+	
+	/// makes the cell a relative cell and sets the relative hight
+	inline void set_relative_height(float height){
+		this->gridWidthType = GridWidthType::Relative;
+		this->relativeHeight = height;
+	}
+	
+	inline void set_dynamic_height(){
+		this->gridWidthType = GridWidthType::Dynamic;
+	}
+	
+	inline void set_minimal_height(size_type minimalHeight) { this->minimalHeight = minimalHeight; }
+	
+	inline void set_maximal_height(size_type maximalHeight) { this->maximalHeight = maximalHeight; }
+	
+	void render(std::string& output) const override;
+	
+	void set_screen_position(ScreenPosition position) override;
+	
+	ScreenPosition get_screen_position() const override;
+	
+	void set_screen_width(ScreenWidth width) override;
+
+	ScreenWidth get_screen_width() const override;
+};
+
+class VerticalGrid : public RenderTrait{
+	
+	using list_type = std::vector<VerticalGridCell>;
+	
+	list_type gridCells;
+	
+	ScreenPosition screenPosition;
+	ScreenWidth screenWidth;
+	
+	bool centering = false;
+	
+public:
+	
+	using pointer = VerticalGridCell::pointer;
+	using unique_pointer = VerticalGridCell::unique_pointer;
+	
+	using size_type = list_type::size_type;
+	using iterator = list_type::iterator;
+	using const_iterator = list_type::const_iterator;
+	
+	inline VerticalGrid(ScreenPosition screenPosition, ScreenWidth screenWidth) : 
+		screenPosition(screenPosition),
+		screenWidth(screenWidth)
+	{}
+	
+	/// inserts an element into the grid with an absolute height
+	void insert_absolute(const_iterator pos, unique_pointer&& element, ScreenWidth::size_type height);
+	void insert_absolute(const_iterator pos, pointer&& element, ScreenWidth::size_type height);
+	void insert_absolute(size_type pos, unique_pointer&& element, ScreenWidth::size_type height);
+	void insert_absolute(size_type pos, pointer&& element, ScreenWidth::size_type height);
+	
+	/// inserts an element into the grid with a relative height
+	/// note: 	that the relative height does not need to be a percentage, it can be a seen as a 'strength'
+	/// 		if all elements have the relative height of 1 (or generally: the same relative-height), 
+	///			then all will be equally spaced on the screen.
+	void insert_relative(const_iterator pos, unique_pointer&& element, float height);
+	void insert_relative(const_iterator pos, pointer&& element, float height);
+	void insert_relative(size_type pos, unique_pointer&& element, float height);
+	void insert_relative(size_type pos, pointer&& element, float height);
+	
+	void insert_dynamic(const_iterator pos, unique_pointer&& element);
+	void insert_dynamic(const_iterator pos, pointer&& element);
+	void insert_dynamic(size_type pos, unique_pointer&& element);
+	void insert_dynamic(size_type pos, pointer&& element);
+	
+	/// appends an element with an absolute line number
+	void push_back_absolute(unique_pointer&& element, ScreenWidth::size_type height);
+	void push_back_absolute(pointer element, ScreenWidth::size_type height);
+	
+	/// appends an element with a relative line number
+	void push_back_relative(unique_pointer&& element, float height);
+	void push_back_relative(pointer element, float height);
+	
+	void push_back_dynamic(unique_pointer&& element);
+	void push_back_dynamic(pointer element);
+	
+	void render(std::string& outputString) const override;
+	
+	/// sets the position of the object on the screen
+	void set_screen_position(ScreenPosition position) override;
+	
+	/// get the position of the object on the screen
+	ScreenPosition get_screen_position() const override;
+	
+	/// sets the width of the object on the screen
+	void set_screen_width(ScreenWidth width) override;
+	
+	/// get the render width of the object
+	ScreenWidth get_screen_width() const override;
+	
+	
+private:
+
+	ScreenWidth::size_type accumulate_absolute_cell_height() const;
+	float accumulate_relative_cell_height() const;
+	
+	void distribute_cells();
+	
+};
+
+}
