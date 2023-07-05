535381b43eaf828b34264685aca85dded9a5ca2e
Tobias Wallner <tobias.wallner1@gmx.net>
2023-07-05 21:15:51 +0200

    prevent calling outputString.append(count, ' ') to clear the rest of the screen with a negative count which caused the editor to crash

--- modules/cpp-terminal-gui/cpp-terminal-gui/Label.cpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/Label.cpp
@@ -12,25 +12,38 @@ void TermGui::Label::render(std::string& outputString) const {
 	size_type lineNumber = 0;
 	outputString += Term::cursor_move(this->screenPosition.y + lineNumber, this->screenPosition.x);
 	for(;index < this->size() && lineNumber < this->screenWidth.y; (void)++index, (void)++columnNumber){
+		// render styles
 		if(styleItr != styleEnd){
 			if(styleItr->index == index){
 				styleItr->render(outputString);
 				++styleItr;
 			}
 		}
-		if(string[index] == '\t'){
-			const auto tabs_to_print = std::min(4ULL, this->screenWidth.x - columnNumber);
-			outputString.append(tabs_to_print, ' ');
-			columnNumber += tabs_to_print-1;
-		}else if(string[index] == '\n'){
-			outputString.append(this->screenWidth.x - columnNumber, ' '); // clear after end of line
+
+		if (columnNumber < this->screenWidth.x) {
+			// print characters that can be seen on the screen
+			if (string[index] == '\n') {
+				outputString.append(this->screenWidth.x - columnNumber, ' '); // clear after end of line
+				++lineNumber;
+				columnNumber = 0;
+				outputString += Term::cursor_move(this->screenPosition.y + lineNumber, this->screenPosition.x);
+			}if (string[index] == '\t') {
+				const auto tabs_to_print = std::min(4ULL, this->screenWidth.x - columnNumber);
+				outputString.append(tabs_to_print, ' ');
+				columnNumber += tabs_to_print - 1;
+			}else{
+				outputString += string[index].to_std_string_view();
+			}
+		}
+		else if (string[index] == '\n') {
+			// line break that cannot be seen on the screen
 			++lineNumber;
 			columnNumber = 0;
 			outputString += Term::cursor_move(this->screenPosition.y + lineNumber, this->screenPosition.x);
-		}else if(columnNumber < this->screenWidth.x){
-			outputString += string[index].to_std_string_view();
 		}
+		
 	}
+
 	// there could be one last style (probably for resetting styles) after the last character
 	if(styleItr != styleEnd){
 		if(styleItr->index == index){
