52b7fc1e2844b52144dcbe58aba118176d3e7b30
Tobias Wallner <tobias.wallner1@gmx.net>
2023-07-15 18:49:01 +0200

    added a cursor class

---
+++ modules/cpp-terminal-gui/cpp-terminal-gui/TextCursor.cpp
@@ -0,0 +1,149 @@
+// C++ std
+#include <fstream>
+#include <algorithm>
+
+// utf8 string 
+#include <utf8_char.hpp>
+#include <utf8_string.hpp>
+
+// project
+#include "ColorString.hpp"
+#include "TextCursor.hpp"
+
+TermGui::TextCursor::TextCursor(Text* pText,
+				Text::iterator lineIterator,
+				std::int32_t lineIndex,
+				std::int32_t columnIndex,
+				std::int32_t screenColumn) :
+	pText(pText),
+	lineIterator(lineIterator),
+	lineIndex(lineIndex),
+	columnIndex(columnIndex),
+	screenColumn(screenColumn)
+	{}
+
+
+
+void TermGui::TextCursor::move_forward(){
+	if(this->is_end_of_file()){
+		return;
+	}else if(this->is_end_of_line()){
+		this->move_down_to_start_of_line();
+	}else{
+		this->screenColumn += (this->get_char() == '\t') ? this->tabSize : 1;
+		++this->columnIndex;
+	}
+}
+
+void TermGui::TextCursor::move_back(){
+	if(this->is_start_of_file()){
+		return;
+	}else if(this->is_start_of_line()){
+		this->move_up_to_start_of_line();
+	}else{
+		--this->columnIndex;
+		this->screenColumn -= (this->get_char() == '\t') ? this->tabSize : 1;
+	}
+}
+
+void TermGui::TextCursor::move_up_to_start_of_line(){
+	--this->lineIterator;
+	--this->lineIndex;
+	this->columnIndex = 0;
+	this->screenColumn = 0;
+}
+
+void TermGui::TextCursor::move_down_to_start_of_line(){
+	++this->lineIterator;
+	++this->lineIndex;
+	this->columnIndex = 0;
+	this->screenColumn = 0;
+}
+
+void TermGui::TextCursor::move_up(std::int32_t n){
+	if(this->is_start_of_file()){
+		return;
+	}else if(this->is_first_line()){
+		this->move_to_start_of_line();
+	}else{
+		const auto prevScreenColumn = this->screenColumn;
+		for(std::int32_t i = 0; i < n && !this->is_first_line(); ++i){
+			move_up_to_start_of_line();
+		}
+		move_to_screen_column_before(prevScreenColumn);
+	}
+}
+
+void TermGui::TextCursor::move_down(std::int32_t n){
+	if(this->is_end_of_file()){
+		return;
+	}else if(this->is_last_line()){
+		this->move_to_end_of_line();
+	}else{
+		const auto prevScreenColumn = this->screenColumn;
+		for(std::int32_t i = 0; (i < n) && !this->is_last_line(); ++i){
+			move_down_to_start_of_line();
+		}
+		move_to_screen_column_before(prevScreenColumn);
+	}
+}
+
+void TermGui::TextCursor::move_to_screen_column_before(std::int32_t wantedScreenColumn){
+	// move foreward
+	while(!this->is_end_of_line() && this->screenColumn < wantedScreenColumn){	
+		this->screenColumn += (this->get_char() == '\t') ? this->tabSize : 1;
+		++this->columnIndex;
+	}
+	
+	// move backward
+	while(!this->is_start_of_line() && this->screenColumn > wantedScreenColumn){	
+		--this->columnIndex;
+		this->screenColumn -= (this->get_char() == '\t') ? this->tabSize : 1;
+	}
+}
+
+void TermGui::TextCursor::move_to_screen_column_after(std::int32_t wantedScreenColumn){
+	// move backward
+	while(!this->is_start_of_line() && this->screenColumn > wantedScreenColumn){	
+		--this->columnIndex;
+		this->screenColumn -= (this->get_char() == '\t') ? this->tabSize : 1;
+	}
+	
+	// move foreward
+	while(!this->is_end_of_line() && this->screenColumn < wantedScreenColumn){	
+		this->screenColumn += (this->get_char() == '\t') ? this->tabSize : 1;
+		++this->columnIndex;
+	}
+}
+
+
+
+void TermGui::TextCursor::move_to_start_of_line(){
+	this->lineIndex = 0;
+	this->columnIndex = 0;
+	this->screenColumn = 0;
+}
+
+void TermGui::TextCursor::move_to_end_of_line(){
+	while(!this->is_end_of_line()){
+		this->screenColumn += (this->get_char() == '\t') ? this->tabSize : 1;
+		++this->columnIndex;
+	}
+}
+
+void TermGui::TextCursor::move_to_start_of_file(){
+	this->lineIterator = this->pText->begin();
+	this->lineIndex = 0;
+	this->columnIndex = 0;
+	this->screenColumn = 0;
+}
+
+void TermGui::TextCursor::move_to_end_of_file(){
+	// TODO: change once line wrapping becomes a thing
+	this->lineIterator = --this->pText->begin();
+	this->lineIndex = this->pText->size();
+	this->columnIndex = 0;
+	this->screenColumn = 0;
+	this->move_to_end_of_line();
+}
+
---
+++ modules/cpp-terminal-gui/cpp-terminal-gui/TextCursor.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+// c++ std
+#include <list>
+
+// project
+
+// project
+#include "FontStyle.hpp"
+#include "ColorString.hpp"
+
+namespace TermGui{
+	
+class TextEditor;
+
+class TextCursor{
+	
+public:
+	using Line = ColorString;		// will be managed by this class in a way so that there are no linebraks in a line
+	using Text = std::list<Line>;	// list of lines
+	
+	Text* pText;
+	Text::iterator lineIterator;
+	std::int32_t lineIndex;
+	std::int32_t columnIndex;
+
+	std::int32_t screenColumn;
+
+	std::int32_t tabSize = 4;
+public:
+	
+	explicit TextCursor(Text* pText,
+				Text::iterator lineIterator,
+				std::int32_t lineIndex,
+				std::int32_t columnIndex = 0,
+				std::int32_t screenColumn = 0);
+	
+	TextCursor(const TextCursor& other) = default;
+	
+	TextCursor& operator=(TextCursor& other) = default;
+
+	inline Text::iterator line_iterator() const {return this->lineIterator;}
+	inline std::int32_t line_index() const {return this->lineIndex;}
+	inline std::int32_t column_index() const {return this->columnIndex;}
+	inline std::int32_t screen_column() const {return this->screenColumn;}
+	
+	inline bool is_start_of_line() const {return this->columnIndex == 0;}
+	inline bool is_end_of_line() const {return this->columnIndex == lineIterator->size();}
+	inline bool is_first_line() const {return this->lineIterator == this->pText->begin();}
+	inline bool is_last_line() const {return this->lineIterator == --this->pText->end();}
+	inline bool is_start_of_file() const {return this->is_first_line() && this->is_start_of_line();}
+	inline bool is_end_of_file() const {return this->is_last_line() && this->is_end_of_line();}
+
+	void move_forward();
+	void move_back();
+	void move_up_to_start_of_line();
+	void move_down_to_start_of_line();
+	void move_up(std::int32_t n = 1);
+	void move_down(std::int32_t n = 1);
+	
+	void move_to_start_of_line();
+	void move_to_end_of_line();
+	void move_to_start_of_file();
+	void move_to_end_of_file();
+	
+	void move_to_screen_column_before(std::int32_t wantedScreenColumn);
+	void move_to_screen_column_after(std::int32_t wantedScreenColumn);
+	
+	inline utf8::Char get_char(){return lineIterator->empty() ? utf8::Char('\0') : this->lineIterator->at(this->columnIndex);}
+	
+	friend inline bool operator==(const TextCursor& l, const TextCursor& r){
+		return l.lineIterator == r.lineIterator && l.columnIndex == r.columnIndex;
+	}
+	friend inline bool operator!=(const TextCursor& l, const TextCursor& r){return !(l == r);}
+
+	friend inline bool operator<(const TextCursor& l, const TextCursor& r){
+		return l.lineIndex < r.lineIndex || (l.lineIndex == r.lineIndex && l.columnIndex < r.columnIndex);
+	}
+	friend inline bool operator<=(const TextCursor& l, const TextCursor& r){return !(l > r);}
+	friend inline bool operator>(const TextCursor& l, const TextCursor& r){return (r < l);}
+	friend inline bool operator>=(const TextCursor& l, const TextCursor& r){return !(l < r);}
+};
+
+}
