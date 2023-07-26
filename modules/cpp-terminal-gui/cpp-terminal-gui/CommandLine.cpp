f7c4b8b4153c8a99a7b4edfaf738ea1415dfd793
Tobias Wallner <tobias.wallner1@gmx.net>
2023-07-26 14:51:25 +0200

    added a Command and a CommandList class

--- modules/cpp-terminal-gui/cpp-terminal-gui/CommandLine.cpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/CommandLine.cpp
@@ -1,21 +1,131 @@
 
+// std c++
+#include <algorithm>
+
+
+// project
 #include "CommandLine.hpp"
 #include "FontStyle.hpp"
 
-TermGui::CommandLine::CommandLine(object_pointer objectPtr, method_type method, ScreenPosition position, ScreenWidth width) : 
+bool TermGui::CommandList::empty() const {return this->commandList.empty();}
+TermGui::CommandList::size_type TermGui::CommandList::size() const {return this->commandList.size();}
+
+void TermGui::CommandList::reserve(TermGui::CommandList::size_type new_cap){
+	this->commandList.reserve(new_cap);
+}
+
+void TermGui::CommandList::insert(const Command& command){
+	const auto pos = std::upper_bound(this->begin(), this->end(), command.name, greater);
+	this->commandList.insert(pos, command);
+}
+
+void TermGui::CommandList::insert(Command&& command){
+	const auto pos = std::upper_bound(this->begin(), this->end(), command.name, greater);
+	this->commandList.insert(pos, std::move(command));
+}
+
+TermGui::CommandList::const_range TermGui::CommandList::find(const utf8::string_view& command) const{
+	auto first = std::lower_bound(this->begin(), this->end(), command, less);
+	auto last = std::upper_bound(first, this->end(), command, greater);
+	return const_range{first, last};
+}
+
+TermGui::CommandList::const_range TermGui::CommandList::find(const std::vector<utf8::string_view>& commands) const {
+	if(commands.empty()){
+		return const_range{this->end(), this->end()};
+	}
+	
+	const_range range{this->begin(), this->end()};
+	
+	auto* list = this;
+	for(const auto& command : commands){
+		range = list->find(command);
+		const auto dist = std::distance(range.first, range.last);
+		if(dist == 0){
+			break;
+		}else if(dist == 1){
+			if(range.first->subCommands.empty()){
+				break;
+			}else{
+				list = &(range.first->subCommands);
+			}
+		}
+	}
+	return range;
+}
+
+bool TermGui::less(const TermGui::Command &a, const utf8::string_view &b){ return a.name < b; }
+bool TermGui::greater(const utf8::string_view &a, const TermGui::Command &b){ 
+	auto lhsItr = a.begin();
+	const auto lhsEnd = a.end();
+	auto rhsItr = b.name.begin();
+	const auto rhsEnd = b.name.end();
+	for(; lhsItr != lhsEnd && rhsItr != rhsEnd; ++lhsItr, (void) ++rhsItr){
+		if(!(*lhsItr > *rhsItr)){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+/// parses a utf8 view of a command string into a vector of string views
+/// returns a vector where each element holds one word or string ("..") of the command
+static std::vector<utf8::string_view> parse_command_string(utf8::string_view commandString){
+	std::vector<utf8::string_view> commandList;
+	auto itr = commandString.cbegin();
+	const auto end = commandString.cend();
+	while(itr != end){
+		//skip whitespaces
+		if(utf8::is_whitespace(*itr) || utf8::is_control(*itr)){
+			// go to next
+			++itr;
+		}else if(*itr == '"'){
+			// find next " or end
+			auto next = itr+1;
+			for (; next != end; ++next) {
+				if (*next == '"') break;
+			}
+			const bool has_quotation_marks = next != end;
+			const auto distance = std::distance(itr + 1, next);
+			const utf8::string_view::const_pointer pChar = &(*(itr + 1));
+			commandList.emplace_back(pChar, distance);
+			itr = next + has_quotation_marks;
+		}else{
+			// find next whitespaces
+			auto next = itr+1;
+			for (; next != end; ++next) {
+				if (utf8::is_whitespace(*next)) break;
+			}
+			const auto distance = std::distance(itr, next);
+			const utf8::string_view::const_pointer pChar = &(*itr);
+			commandList.emplace_back(pChar, distance);
+			itr = next;
+		}
+	}
+	return commandList;
+}
+
+TermGui::CommandLine::CommandLine(object_pointer objectPtr, callback_type enterCallback, ScreenPosition position, ScreenWidth width) : 
 		commandString(), 
 		objectPtr(objectPtr),
-		method(method),
+		enterCallback(enterCallback),
 		position(position),
 		width(width)
 		{}
 
 void TermGui::CommandLine::show_cursor(bool on_off) { this->showCursor = on_off; }
 
+
 void TermGui::CommandLine::clear() {
 	this->move_to_start_of_line();
 	this->commandString.clear();
 	historyItr = commandHistory.cend();
+}
+
+void TermGui::CommandLine::naive_insert(utf8::Char c){
+	this->commandString.insert(cursorIndex, c);
+	this->move_forward();
 }
 
 void TermGui::CommandLine::insert(utf8::Char c) {
@@ -25,17 +135,20 @@ void TermGui::CommandLine::insert(utf8::Char c) {
 	}else if(c == '\b'){
 		this->Delete();
 	}else{
-		this->commandString.insert(cursorIndex, c);
-		this->move_forward();
+		this->naive_insert(c);
 	}
 }
 
 void TermGui::CommandLine::enter() {
-	method(objectPtr, this->commandString);
+	if(enterCallback != nullptr && objectPtr != nullptr){
+		auto commands = parse_command_string (this->commandString);
+		enterCallback(objectPtr, commands);
+	}
+	
 	if(this->commandHistory.empty()){
-		this->commandHistory.push_front(std::move(this->commandString));	
+		this->commandHistory.push_front(this->commandString);	
 	}else if(this->commandHistory.front() != this->commandString){
-		this->commandHistory.push_front(std::move(this->commandString));	
+		this->commandHistory.push_front(this->commandString);	
 	}
 	this->clear();
 }
--- modules/cpp-terminal-gui/cpp-terminal-gui/CommandLine.hpp
+++ modules/cpp-terminal-gui/cpp-terminal-gui/CommandLine.hpp
@@ -6,10 +6,12 @@
 // c++ std
 #include <algorithm>
 #include <list>
+#include <vector>
 
 //project
 #include "RenderTrait.hpp"
 #include "EditTrait.hpp"
+#include "ColorString.hpp"
 
 #include <cpp-terminal/cursor.hpp>
 
@@ -19,21 +21,92 @@
 
 namespace TermGui{
 
+struct Command;
+
+/// sorted list of commands
+class CommandList{
+	using list_type = std::vector<Command>;
+	list_type commandList;
+	
+public:
+	
+	using value_type = list_type::value_type;
+	using allocator_type = list_type::allocator_type;
+	using size_type = list_type::size_type;
+	using difference_type = list_type::difference_type;
+	using reference = list_type::reference;
+	using const_reference = list_type::const_reference;
+	using pointer = list_type::pointer;
+	using const_pointer = list_type::const_pointer;
+	using iterator = list_type::iterator;
+	using const_iterator = list_type::const_iterator;
+	
+	struct range{iterator first; iterator last;};
+	struct const_range{const_iterator first; const_iterator last;};
+	
+	inline iterator begin() {return this->commandList.begin();}
+	inline const_iterator begin() const {return this->commandList.begin();}
+	inline const_iterator cbegin() const {return this->commandList.cbegin();}
+	
+	inline iterator end() {return this->commandList.end();}
+	inline const_iterator end() const {return this->commandList.end();}
+	inline const_iterator cend() const {return this->commandList.cend();}
+	
+	bool empty() const;
+	size_type size() const;
+	
+	void reserve(size_type new_cap);
+	
+	void insert(const Command& command);
+	void insert(Command&& command);
+	
+	const_range find(const utf8::string_view& command) const;
+	const_range find(const std::vector<utf8::string_view>& commands) const;
+	
+};
+
+/// command element
+struct Command{
+	using object_pointer = void*;
+	using callback_type = void(*)(object_pointer self, const std::vector<utf8::string_view>& command);
+	
+	struct Flag{
+		utf8::string name;
+		ColorString info;
+	};
+	
+	utf8::string name;
+	ColorString info;
+	std::vector<Flag> Flags;
+	CommandList subCommands;
+	
+	object_pointer objectPtr;
+	callback_type callbackFn;
+	
+	inline void call(const std::vector<utf8::string_view>& command) const { 
+		if(callbackFn != nullptr) callbackFn(objectPtr, command); 
+	}
+};
+
+
+bool less(const Command &a, const utf8::string_view &b);
+bool greater(const utf8::string_view &a, const Command &b);
+
 /// offers an editable string with a callback function that gets executed on enter.
 /// On construction offer a pointer to the object that should be notified and a 
-/// will move the command string into the provided method
+/// will move the command string into the provided enterCallback
 class CommandLine : public RenderTrait, public EditTrait{
 public:
 	using string_type = utf8::string;
 	using value_type = string_type::value_type;
 	using size_type = string_type::size_type;
 	using object_pointer = void*;
-	using method_type = void(*)(object_pointer self, utf8::string_view);
-	
+	using callback_type = void(*)(object_pointer self, const std::vector<utf8::string_view>& commands);
+
 private:
 	string_type commandString;
 	object_pointer objectPtr; // pointer cannot be changed after construction
-	method_type method; // method cannot be changed after construction
+	callback_type enterCallback; // enterCallback cannot be changed after construction
 	
 	ScreenPosition position;
 	ScreenWidth width;
@@ -51,7 +124,7 @@ private:
 	
 public:
 	
-	CommandLine(object_pointer objectPtr, method_type method, ScreenPosition position = {0,0}, ScreenWidth width = {0,0});
+	CommandLine(object_pointer objectPtr, callback_type enterCallback, ScreenPosition position = {0,0}, ScreenWidth width = {0,0});
 		
 	
 	CommandLine(const CommandLine&) = default;
@@ -66,11 +139,10 @@ public:
 	
 	void clear() override;
 	
+	void naive_insert(utf8::Char c);
 	void insert(utf8::Char c) override;
-	inline bool insert(const char* str){return this->EditTrait::insert(str);}
-	inline bool insert(const char* str, size_t size){return this->EditTrait::insert(str, size);}
 	
-	/// calls the provided method, passes the command string and clears the command line
+	/// calls the provided enterCallback, passes the command string and clears the command line
 	void enter() override;
 	void erase() override;
 	void Delete() override;
