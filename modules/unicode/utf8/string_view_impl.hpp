9c25f37537754f3a7f7d398fa2c4f3e726fa9f25
Tobias Wallner <tobias.wallner1@gmx.net>
2023-10-09 00:04:02 +0200

    switched the construction hierarchy

--- modules/unicode/utf8/string_view_impl.hpp
+++ modules/unicode/utf8/string_view_impl.hpp
@@ -10,12 +10,38 @@
 
 namespace utf8{
 
+
+constexpr const_string_view::const_string_view(const utf8::string& str) : _begin(str.begin()), _end(str.end()) {}
+constexpr const_string_view::const_string_view(const char* str) : _begin(str), _end(strend(str)){}
+constexpr const_string_view::const_string_view(const char* first, const char* last) : _begin(first), _end(last){}
+constexpr const_string_view::const_string_view(const_string_view::const_iterator first, const_string_view::const_iterator last) : _begin(first), _end(last){}
+constexpr const_string_view::const_string_view(std::string_view other) : _begin(&*other.begin()), _end(&*other.end()){}
+constexpr const_string_view& const_string_view::operator=(std::string_view other) {this->_begin = &*other.begin(); this->_end = &*other.end();return *this;}
+
+constexpr const_string_view::const_iterator const_string_view::begin() const {return _begin;}
+constexpr const_string_view::const_iterator const_string_view::cbegin() const {return _begin;}
+
+constexpr const_string_view::const_iterator const_string_view::end() const {return _end;}
+constexpr const_string_view::const_iterator const_string_view::cend() const {return _end;}
+
+constexpr const_string_view::size_type const_string_view::size() const {return utf8::distance(this->begin(), this->end());}
+constexpr const_string_view::size_type const_string_view::length() const {return utf8::distance(this->begin(), this->end());}
+
+constexpr bool const_string_view::empty() const {return this->begin() == this->end();}
+
+constexpr void const_string_view::swap(const_string_view& other){
+	std::swap(this->_begin, other._begin);
+	std::swap(this->_end, other._end);
+}
+
+constexpr const_string_view::operator std::string_view() const {return std::string_view(&*this->cbegin(), &*this->cend());}
+
+
+
 constexpr string_view::string_view(utf8::string* str, const char* first, const char* last) : _str(str), _begin(first), _end(last){}
 constexpr string_view::string_view(utf8::string* str, const_iterator first, const_iterator last) : _str(str), _begin(first), _end(last){}
 constexpr string_view::string_view(string_view::iterator first, string_view::iterator last) : _str(first.container()), _begin(first), _end(last){}
 constexpr string_view::string_view(utf8::string& str) : _str(&str), _begin(str.cbegin()), _end(str.cend()){}
-constexpr string_view::string_view(const string_view& other) = default;
-constexpr string_view& string_view::operator=(const string_view& other) = default;
 
 constexpr string_view::iterator string_view::begin() {return _str->unconst(_begin);}
 constexpr string_view::const_iterator string_view::begin() const {return _begin;}
@@ -37,32 +63,7 @@ constexpr void string_view::swap(string_view& other){
 }
 
 constexpr string_view::operator std::string_view() const {return std::string_view(&*this->cbegin(), &*this->cend());}
+constexpr string_view::operator utf8::const_string_view() const{return utf8::const_string_view(&*this->cbegin(), &*this->cend());}
 
-constexpr const_string_view::const_string_view(const utf8::string& str) : _begin(str.begin()), _end(str.end()) {}
-constexpr const_string_view::const_string_view(const char* str) : _begin(str), _end(strend(str)){}
-constexpr const_string_view::const_string_view(const char* first, const char* last) : _begin(first), _end(last){}
-constexpr const_string_view::const_string_view(const_string_view::const_iterator first, const_string_view::const_iterator last) : _begin(first), _end(last){}
-constexpr const_string_view::const_string_view(const const_string_view& other) = default;
-constexpr const_string_view& const_string_view::operator=(const const_string_view& other) = default;
-constexpr const_string_view::const_string_view(const string_view& other) : _begin(other.begin()), _end(other.end()){}
-constexpr const_string_view& const_string_view::operator=(const string_view& other) {this->_begin = other.begin();this->_end = other.end();return *this;}
-
-constexpr const_string_view::const_iterator const_string_view::begin() const {return _begin;}
-constexpr const_string_view::const_iterator const_string_view::cbegin() const {return _begin;}
-
-constexpr const_string_view::const_iterator const_string_view::end() const {return _end;}
-constexpr const_string_view::const_iterator const_string_view::cend() const {return _end;}
-
-constexpr const_string_view::size_type const_string_view::size() const {return utf8::distance(this->begin(), this->end());}
-constexpr const_string_view::size_type const_string_view::length() const {return utf8::distance(this->begin(), this->end());}
-
-constexpr bool const_string_view::empty() const {return this->begin() == this->end();}
-
-constexpr void const_string_view::swap(const_string_view& other){
-	std::swap(this->_begin, other._begin);
-	std::swap(this->_end, other._end);
-}
-
-constexpr const_string_view::operator std::string_view() const {return std::string_view(&*this->cbegin(), &*this->cend());}
 
 }
--- modules/unicode/utf8/string_view_type.hpp
+++ modules/unicode/utf8/string_view_type.hpp
@@ -9,6 +9,44 @@
 
 namespace utf8{
 
+class const_string_view{
+	public:
+	using size_type = utf8::string::size_type;
+	using iterator = utf8::string_iterator;
+	using const_iterator = utf8::string_const_iterator;
+	using reference = utf8::char_reference;
+	using const_reference = utf8::char_const_reference;
+	
+private:
+	const_iterator _begin;
+	const_iterator _end;
+	
+public:
+	constexpr const_string_view(const const_string_view& other) = default;
+	constexpr const_string_view& operator=(const const_string_view& other) = default;
+	constexpr const_string_view(const char* str);
+	constexpr const_string_view(const char* first, const char* last);
+	constexpr const_string_view(const_iterator first, const_iterator last);
+	constexpr const_string_view(const utf8::string& str);
+	constexpr const_string_view(std::string_view str);
+	constexpr const_string_view& operator=(std::string_view other);
+	
+	
+	constexpr const_iterator begin() const;
+	constexpr const_iterator cbegin() const;
+	
+	constexpr const_iterator end() const;
+	constexpr const_iterator cend() const;
+	
+	constexpr size_type size() const;
+	constexpr size_type length() const;
+
+	constexpr bool empty() const;
+	constexpr void swap(const_string_view& other);
+	
+	constexpr operator std::string_view() const;
+};
+
 class string_view{
 public:
 	using size_type = utf8::string::size_type;
@@ -23,12 +61,12 @@ private:
 	const_iterator _end;
 	
 public:
+	constexpr string_view(const string_view& other) = default;
+	constexpr string_view& operator=(const string_view& other) = default;
 	constexpr string_view(utf8::string* str, const char* first, const char* last);
 	constexpr string_view(utf8::string* str, const_iterator first, const_iterator last);
 	constexpr string_view(iterator first, iterator last);
-	constexpr string_view(const string_view& other);
 	constexpr string_view(utf8::string& str);
-	constexpr string_view& operator=(const string_view& other);
 	
 	constexpr iterator begin();
 	constexpr const_iterator begin() const;
@@ -46,43 +84,9 @@ public:
 	constexpr void swap(string_view& other);
 	
 	constexpr operator std::string_view() const;
+	constexpr operator utf8::const_string_view() const;
 };
 
-class const_string_view{
-	public:
-	using size_type = utf8::string::size_type;
-	using iterator = utf8::string_iterator;
-	using const_iterator = utf8::string_const_iterator;
-	using reference = utf8::char_reference;
-	using const_reference = utf8::char_const_reference;
-	
-private:
-	const_iterator _begin;
-	const_iterator _end;
-	
-public:
-	constexpr const_string_view(const char* str);
-	constexpr const_string_view(const char* first, const char* last);
-	constexpr const_string_view(const_iterator first, const_iterator last);
-	constexpr const_string_view(const utf8::string& str);
-	constexpr const_string_view(const string_view& other);
-	constexpr const_string_view& operator=(const string_view& other);
-	constexpr const_string_view(const const_string_view& other);
-	constexpr const_string_view& operator=(const const_string_view& other);
-	
-	constexpr const_iterator begin() const;
-	constexpr const_iterator cbegin() const;
-	
-	constexpr const_iterator end() const;
-	constexpr const_iterator cend() const;
-	
-	constexpr size_type size() const;
-	constexpr size_type length() const;
 
-	constexpr bool empty() const;
-	constexpr void swap(const_string_view& other);
-	
-	constexpr operator std::string_view() const;
-};
 
 }
